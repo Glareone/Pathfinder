8e46ea3830e125b55605f12248af1f06f6cbf851
Sergey Babitsky <babitsky.sergey@gmail.com>
2014-01-06 16:34:24 +0300

    Adding missing files

---
+++ Pathfinder.Engine/GameEngineParameters.cs
@@ -0,0 +1,17 @@
+﻿using Pathfinder.Domain.Entities;
+
+namespace Pathfinder.Engine
+{
+    public class GameEngineParameters
+    {
+        /// <summary>
+        /// Map to play
+        /// </summary>
+        public Map Map { get; set; }
+
+        /// <summary>
+        /// GamePlayers collection
+        /// </summary>
+        public GamePlayer[] GamePlayers { get; set; }
+    }
+}
